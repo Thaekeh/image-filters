667a96bc1bb41e3b4ed5e081f5cc98ab7f0c4f2a
Thaeke Hekkenberg <thaekeh@gmail.com>
2022-04-08 18:52:42 +0200

    Finished medianFilter and some example pics

--- scripts/medianFilter.py
+++ scripts/medianFilter.py
@@ -1,6 +1,9 @@
 import cv2
-img = cv2.imread('./images/kolibri.jpg', flags=cv2.IMREAD_COLOR)
-print(img[0][0])
+import numpy as np
+
+filterWindowSize = 1
+imgName = 'grainy-2.jpg'
+img = cv2.imread(f"./images/{imgName}", flags=cv2.IMREAD_COLOR)
 
 def getSurroundingValues(image, row, column, windowSize):
     returnList = []
@@ -8,45 +11,44 @@ def getSurroundingValues(image, row, column, windowSize):
     columnIndexList = listFromMinusNtoN(windowSize)
     for rowIndex in rowIndexList:
         currentRow = row + rowIndex
-        if currentRow < 0:
+        if currentRow < 0 or currentRow > len(img) - 1:
             continue
         for columnIndex in columnIndexList:
             currentColumn = column + columnIndex
-            if currentColumn < 0:
+            if currentColumn < 0 or currentColumn > len(img[0]) - 1:
                 continue
             returnList.append(img[currentRow][currentColumn])
 
 
     return returnList
 
-    
+
 def listFromMinusNtoN(n):
     return list(range(-n,n + 1))
 
 def getMedianFromList(pixelList):
     medianValues = [[], [], []]
+    medians = []
     for pixel in pixelList:
         index = 0
         for value in pixel:
             medianValues[index].append(value)
             index += 1
-    print(medianValues)
+    for valueIndex in range(len(medianValues)):
+        median = np.median(medianValues[valueIndex])
+        medians.append(median)
+    return medians
 
 
 
 for row in range(len(img)):
-    if row == 1:
-        break
     for pixel in range(len(img[row])):
-        # get surrounding values
-        if pixel == 1:
-            break
-        valuesList = getSurroundingValues(img, row, pixel, 1)
-        median = getMedianFromList(valuesList)
+        valuesList = getSurroundingValues(img, row, pixel, filterWindowSize)
+        medians = getMedianFromList(valuesList)
+        img[row][pixel] = medians
 
-# getSurroundingValues(img, 0, 0, 1)
 
-# cv2.imwrite('./result.jpg', img)
+cv2.imwrite(f"blurred-{imgName}", img)
 
 
 
